9c34b9567219a561c4435d208c7ed0281bbed0a1
ks <kspark607@gmail.com>
2022-03-28 17:07:04 +0900

    Modify: settings.py db config 수정

--- SOTJIMAYO/settings.py
+++ SOTJIMAYO/settings.py
@@ -80,12 +80,7 @@ WSGI_APPLICATION = 'SOTJIMAYO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
