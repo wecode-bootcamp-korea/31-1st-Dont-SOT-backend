a1b4544fbc11a6a0ca1aa61a241fa8fcb887639b
ks <kspark607@gmail.com>
2022-03-31 15:06:19 +0900

    Modify: migrations파일 초기화, product models.py username unique=True

--- products/models.py
+++ products/models.py
@@ -16,7 +16,7 @@ class Category(models.Model):
 
 
 class Product(models.Model): 
-    name             = models.CharField(max_length = 30, null = True)
+    name             = models.CharField(max_length = 30)
     price            = models.DecimalField(max_digits = 10, decimal_places = 2)
     description      = models.TextField()
     calory           = models.DecimalField(max_digits = 10, decimal_places = 2, null = True)
