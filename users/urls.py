fe4da8d65e8eb0baf4a10242e9b92f8c1381a9c8
ks <kspark607@gmail.com>
2022-04-01 15:59:46 +0900

    Modify: import 에러 수정, urls.py dictionary를 list로 수정

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import SignupView, IdcheckView
+from .views import SignupView, IdcheckView, SignInView
 
-urlpatterns = {
+urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/signup/idcheck', IdcheckView.as_view()),
     path('/signin', SignInView.as_view()),
-}
+]
